c390c31bd3146b2f386cfd361447cbc309593d54
OmarMohamed <mo.omar753@gmail.com>
2024-11-13 10:45:06 +0200

    Updated ProductController with recent changes

--- src/main/java/org/example/ecomerce/Controller/Product/ProductController.java
+++ src/main/java/org/example/ecomerce/Controller/Product/ProductController.java
@@ -10,7 +10,6 @@ import org.example.ecomerce.Response.ApiResponse;
 import org.example.ecomerce.Service.Product.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
